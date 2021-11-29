d33664c905d75076ae92e99f0ef8d97ae07242e6
Bartłomiej Wierciński <b.wiercinski@ocado.com>
2021-11-30 00:00:12 +0100

    add sttp-oauth2-cache-cats module with CE3 support

---
+++ oauth2-cache-cats/src/test/scala/com/ocadotechnology/sttp/oauth2/cache/cats/CatsRefExpiringCacheSpec.scala
@@ -0,0 +1,63 @@
+package com.ocadotechnology.sttp.oauth2.cache.cats
+
+import cats.effect.IO
+import cats.effect.kernel.Outcome.Succeeded
+import cats.effect.testkit.TestContext
+import cats.effect.testkit.TestInstances
+import org.scalatest.Assertion
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+import scala.concurrent.duration._
+
+class CatsRefExpiringCacheSpec extends AnyWordSpec with Matchers with TestInstances {
+  private implicit val ticker: Ticker = Ticker(TestContext())
+
+  private val someKey = "key"
+  private val someValue = 1
+
+  def runTest(test: IO[Assertion]): Assertion =
+    unsafeRun(test) match {
+      case Succeeded(Some(assertion)) => assertion
+      case wrongResult                => fail(s"Test should finish successfully. Instead ended with $wrongResult")
+    }
+
+  "Cache" should {
+    "return nothing on empty cache" in unsafeRun {
+      for {
+        cache <- CatsRefExpiringCache[IO, String, Int]
+        value <- cache.get(someKey)
+      } yield value
+    }.shouldBe(Succeeded(Some(None)))
+
+    "store and retrieve value immediately" in unsafeRun {
+      for {
+        cache <- CatsRefExpiringCache[IO, String, Int]
+        now   <- IO.realTimeInstant
+        _     <- cache.put(someKey, someValue, now.plusSeconds(60))
+        value <- cache.get(someKey)
+      } yield value
+    }.shouldBe(Succeeded(Some(Some(someValue))))
+
+    "return value right before expiration boundary" in unsafeRun {
+      for {
+        cache <- CatsRefExpiringCache[IO, String, Int]
+        now   <- IO.realTimeInstant
+        _     <- cache.put(someKey, someValue, now.plusSeconds(60))
+        _     <- IO.sleep(60.seconds - 1.nano)
+        value <- cache.get(someKey)
+      } yield value
+    }.shouldBe(Succeeded(Some(Some(someValue))))
+
+    "not return value if expired" in unsafeRun {
+      for {
+        cache <- CatsRefExpiringCache[IO, String, Int]
+        now   <- IO.realTimeInstant
+        _     <- cache.put(someKey, someValue, now.plusSeconds(60))
+        _     <- IO.sleep(60.seconds)
+        value <- cache.get(someKey)
+      } yield value
+    }.shouldBe(Succeeded(Some(None)))
+
+  }
+}

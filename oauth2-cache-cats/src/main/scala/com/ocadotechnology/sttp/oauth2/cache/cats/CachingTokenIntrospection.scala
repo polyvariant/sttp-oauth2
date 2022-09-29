d56d1afac2e20182bbb4e703ad48ea8d2b6c0a48
Michał Pawlik <michal.pawlik@ocado.com>
2022-09-29 14:27:14 +0200

    add apply in companion object

--- oauth2-cache-cats/src/main/scala/com/ocadotechnology/sttp/oauth2/cache/cats/CachingTokenIntrospection.scala
+++ oauth2-cache-cats/src/main/scala/com/ocadotechnology/sttp/oauth2/cache/cats/CachingTokenIntrospection.scala
@@ -3,6 +3,7 @@ package com.ocadotechnology.sttp.oauth2.cache.cats
 import cats.effect.kernel.MonadCancelThrow
 import cats.data.OptionT
 import cats.effect.kernel.Clock
+import cats.effect.kernel.Concurrent
 import cats.implicits._
 import com.ocadotechnology.sttp.oauth2.Introspection.TokenIntrospectionResponse
 import com.ocadotechnology.sttp.oauth2.Secret
@@ -14,10 +15,10 @@ import scala.concurrent.duration._
 import cats.effect.std.Semaphore
 
 final case class CachingTokenIntrospection[F[_]: Clock: MonadCancelThrow](
-  introspection: TokenIntrospection[F],
+  delegate: TokenIntrospection[F],
   semaphore: Semaphore[F],
   cache: ExpiringCache[F, Secret[String], TokenIntrospectionResponse],
-  defaultExpirationTime: FiniteDuration = 10.minutes // should this be instant? seems supported on Scala.js
+  defaultExpirationTime: FiniteDuration // should this be instant? seems supported on Scala.js
 ) extends TokenIntrospection[F] {
 
   override def introspect(token: Secret[String]): F[TokenIntrospectionResponse] =
@@ -38,7 +39,7 @@ final case class CachingTokenIntrospection[F[_]: Clock: MonadCancelThrow](
     // semaphore prevents concurrent token introspection call
     semaphore
       .permit
-      .surround(introspection.introspect(token))
+      .surround(delegate.introspect(token))
       .flatTap { response =>
         cache.put(token, response, responseExpirationOrDefault(response))
       }
@@ -53,4 +54,13 @@ final case class CachingTokenIntrospection[F[_]: Clock: MonadCancelThrow](
 
 }
 
-object CachingTokenIntrospection {}
+object CachingTokenIntrospection {
+
+  def apply[F[_]: Concurrent: Clock](
+    delegate: TokenIntrospection[F],
+    cache: ExpiringCache[F, Secret[String], TokenIntrospectionResponse],
+    defaultExpirationTime: FiniteDuration = 10.minutes
+  ): F[CachingTokenIntrospection[F]] =
+    Semaphore[F](n = 1).map(new CachingTokenIntrospection[F](delegate, _, cache, defaultExpirationTime))
+
+}
